c83f56e4eef03f640665b22f7b342b0d11d853e7
Troy Abbott <troy.abs28@gmail.com>
2025-04-16 13:15:49 +1200

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Lusitana, Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Add the required weights here
+});
+
+export const inter = Inter({
+  subsets: ['latin'],
+});
